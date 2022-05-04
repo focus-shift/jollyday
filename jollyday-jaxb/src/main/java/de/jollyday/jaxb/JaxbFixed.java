bf71296c61c67573f507c6d1ba01ecb9f55d9608
Tobias Schneider <Tobias.Schneider@Tobsch.org>
2022-05-04 11:34:02 +0200

    jaxbFixed to fixed from mapping

--- jollyday-jaxb/src/main/java/de/jollyday/jaxb/JaxbFixed.java
+++ jollyday-jaxb/src/main/java/de/jollyday/jaxb/JaxbFixed.java
@@ -16,48 +16,48 @@ import java.util.stream.Collectors;
 public class JaxbFixed implements de.jollyday.spi.Fixed {
 
   private final XMLUtil xmlUtil = new XMLUtil();
-  private final de.jollyday.jaxb.mapping.Fixed jaxbFixed;
+  private final de.jollyday.jaxb.mapping.Fixed fixed;
 
-  public JaxbFixed(de.jollyday.jaxb.mapping.Fixed jaxbFixed) {
-    this.jaxbFixed = jaxbFixed;
+  public JaxbFixed(de.jollyday.jaxb.mapping.Fixed fixed) {
+    this.fixed = fixed;
   }
 
   @Override
   public MonthDay day() {
-    return MonthDay.of(xmlUtil.getMonth(jaxbFixed.getMonth()), jaxbFixed.getDay());
+    return MonthDay.of(xmlUtil.getMonth(fixed.getMonth()), fixed.getDay());
   }
 
   @Override
   public String descriptionPropertiesKey() {
-    return jaxbFixed.getDescriptionPropertiesKey();
+    return fixed.getDescriptionPropertiesKey();
   }
 
   @Override
   public HolidayType officiality() {
-    return jaxbFixed.getLocalizedType() == null
+    return fixed.getLocalizedType() == null
       ? HolidayType.OFFICIAL_HOLIDAY
-      : HolidayType.valueOf(jaxbFixed.getLocalizedType().name());
+      : HolidayType.valueOf(fixed.getLocalizedType().name());
   }
 
   @Override
   public Year validFrom() {
-    return Year.of(jaxbFixed.getValidFrom());
+    return Year.of(fixed.getValidFrom());
   }
 
   @Override
   public Year validTo() {
-    return Year.of(jaxbFixed.getValidTo());
+    return Year.of(fixed.getValidTo());
   }
 
   @Override
   public YearCycle cycle() {
-    return jaxbFixed.getEvery() == null
+    return fixed.getEvery() == null
       ? YearCycle.EVERY_YEAR
-      : YearCycle.valueOf(jaxbFixed.getEvery());
+      : YearCycle.valueOf(fixed.getEvery());
   }
 
   @Override
   public List<de.jollyday.spi.MovingCondition> conditions() {
-    return jaxbFixed.getMovingCondition().stream().map(JaxbMovingCondition::new).collect(Collectors.toList());
+    return fixed.getMovingCondition().stream().map(JaxbMovingCondition::new).collect(Collectors.toList());
   }
 }
