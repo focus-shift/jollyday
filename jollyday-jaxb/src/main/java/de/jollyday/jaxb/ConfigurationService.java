b3f6385d4412f36dc2d9a373a87b173f0f555b19
Tobias Schneider <Tobias.Schneider@Tobsch.org>
2022-05-04 10:53:26 +0200

    hard configuration of jaxb configuration service

--- jollyday-jaxb/src/main/java/de/jollyday/jaxb/ConfigurationService.java
+++ jollyday-jaxb/src/main/java/de/jollyday/jaxb/ConfigurationService.java
@@ -1,6 +1,7 @@
 package de.jollyday.jaxb;
 
 import de.jollyday.ManagerParameter;
+import de.jollyday.datasource.ConfigurationDataSource;
 
 import java.io.InputStream;
 import java.net.URL;
@@ -10,14 +11,14 @@ import java.net.URL;
  * @version $
  * @since 15.03.20
  */
-public class ConfigurationService implements de.jollyday.spi.ConfigurationService {
+public class ConfigurationService implements ConfigurationDataSource {
 
   private final XMLUtil xmlUtil = new XMLUtil();
 
   @Override
   public de.jollyday.spi.Configuration getConfiguration(ManagerParameter parameter) {
-    URL resourceUrl = parameter.createResourceUrl();
-    try (InputStream inputStream = resourceUrl.openStream()) {
+    final URL resourceUrl = parameter.createResourceUrl();
+    try (final InputStream inputStream = resourceUrl.openStream()) {
       return new Configuration(xmlUtil.unmarshallConfiguration(inputStream));
     } catch (Exception e) {
       throw new IllegalStateException("Cannot instantiate configuration from URL '" + resourceUrl + "'.", e);
