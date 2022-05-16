cb1c61e2f99308497ec9e91b0269a86481b5540c
Tobias Schneider <Tobias.Schneider@Tobsch.org>
2022-05-16 14:56:30 +0200

    Reactivate XMLValidationTest

--- jollyday-core/src/test/java/de/focus_shift/XMLValidationTest.java
+++ jollyday-core/src/test/java/de/focus_shift/XMLValidationTest.java
@@ -1,8 +1,7 @@
-package de.focus_shift.tests;
+package de.focus_shift;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.xmlunit.validation.Languages;
 import org.xmlunit.validation.ValidationResult;
 import org.xmlunit.validation.Validator;
 
@@ -15,6 +14,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.xmlunit.validation.Languages.W3C_XML_SCHEMA_NS_URI;
 
 class XMLValidationTest {
 
@@ -22,14 +22,14 @@ class XMLValidationTest {
   private Validator validator;
 
   @BeforeEach
-  void setUp() throws Exception {
-    schemaFile = new File("src/main/xsd/Holiday.xsd");
-    validator = Validator.forLanguage(Languages.W3C_XML_SCHEMA_NS_URI);
+  void setUp() {
+    schemaFile = new File("src/main/resources/Holiday.xsd");
+    validator = Validator.forLanguage(W3C_XML_SCHEMA_NS_URI);
     validator.setSchemaSource(new StreamSource(schemaFile));
   }
 
   @Test
-  void testSchemaIsValid() throws Exception {
+  void testSchemaIsValid() {
     assertTrue(schemaFile.exists(), "Schema file " + schemaFile + " does not exist.");
     final ValidationResult validationResult = validator.validateSchema();
     assertTrue(validationResult.isValid(), "Schema '" + schemaFile + "' is not valid: " + validationResult.getProblems());
@@ -42,7 +42,6 @@ class XMLValidationTest {
   }
 
   private void validateHolidayFile(Path path) {
-
     try {
       final ValidationResult validationResult = validator.validateInstance(new StreamSource(new FileInputStream(path.toFile())));
       assertTrue(validationResult.isValid(), "Validation of holiday file '" + path + " failed: " + validationResult.getProblems());
