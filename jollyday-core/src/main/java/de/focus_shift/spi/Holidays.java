a25f237db79235aac98df01f3315f62496af7f01
Tobias Schneider <Tobias.Schneider@Tobsch.org>
2022-05-18 22:24:22 +0200

    Remove not used hebrew holiday

--- jollyday-core/src/main/java/de/focus_shift/spi/Holidays.java
+++ jollyday-core/src/main/java/de/focus_shift/spi/Holidays.java
@@ -20,9 +20,6 @@ public interface Holidays {
 
   List<FixedWeekdayRelativeToFixed> fixedWeekdayRelativeToFixed();
 
-  // TODO: ? is this used anywhere?
-  // Stream<HebrewHoliday> hebrewHolidays();
-
   List<EthiopianOrthodoxHoliday> ethiopianOrthodoxHolidays();
 
   List<RelativeToEasterSunday> relativeToEasterSunday();
