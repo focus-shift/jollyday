df0bbeb35ca683374e993fad0f92ca2397e9921b
Tobias Schneider <Tobias.Schneider@Tobsch.org>
2023-03-26 18:53:22 +0200

    Add tests for calendar to date conversion

--- jollyday-core/src/main/java/de/focus_shift/parser/functions/CalendarToLocalDate.java
+++ jollyday-core/src/main/java/de/focus_shift/parser/functions/CalendarToLocalDate.java
@@ -4,10 +4,14 @@ import java.time.LocalDate;
 import java.util.Calendar;
 import java.util.function.Function;
 
+import static java.util.Calendar.DAY_OF_MONTH;
+import static java.util.Calendar.MONTH;
+import static java.util.Calendar.YEAR;
+
 public class CalendarToLocalDate implements Function<Calendar, LocalDate> {
 
   @Override
   public LocalDate apply(Calendar calendar) {
-    return LocalDate.of(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH) + 1, calendar.get(Calendar.DAY_OF_MONTH));
+    return LocalDate.of(calendar.get(YEAR), calendar.get(MONTH) + 1, calendar.get(DAY_OF_MONTH));
   }
 }
