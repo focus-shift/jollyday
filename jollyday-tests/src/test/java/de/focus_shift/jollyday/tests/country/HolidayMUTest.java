c11eb38635ecafe7eab68b9fcd78682c9ca612f0
XSpielinbox <55600187+xspielinbox@users.noreply.github.com>
2023-08-24 12:18:38 +0200

    Add Mauritius public holidays

---
+++ jollyday-tests/src/test/java/de/focus_shift/jollyday/tests/country/HolidayMUTest.java
@@ -0,0 +1,67 @@
+package de.focus_shift.jollyday.tests.country;
+
+import de.focus_shift.jollyday.core.Holiday;
+import de.focus_shift.jollyday.core.HolidayCalendar;
+import de.focus_shift.jollyday.core.HolidayManager;
+import de.focus_shift.jollyday.core.ManagerParameters;
+import de.focus_shift.jollyday.tests.country.base.AbstractCountryTestBase;
+import de.focus_shift.jollyday.core.util.CalendarUtil;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Locale;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class HolidayMUTest extends AbstractCountryTestBase {
+
+  private static final String ISO_CODE = "mu";
+
+  private final CalendarUtil calendarUtil = new CalendarUtil();
+
+  @ParameterizedTest
+  @ValueSource(ints = {2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024})
+  void testManagerVGStructure(final int year) {
+    validateCalendarData(ISO_CODE, year, true);
+  }
+
+  @Test
+  void testManagerVGInterval() {
+    try {
+      final HolidayManager instance = HolidayManager.getInstance(ManagerParameters.create(HolidayCalendar.MAURITIUS, null));
+      final LocalDate startDateInclusive = calendarUtil.create(2022, 10, 1);
+      final LocalDate endDateInclusive = calendarUtil.create(2023, 1, 31);
+      final Set<Holiday> holidays = instance.getHolidays(startDateInclusive, endDateInclusive);
+      final List<LocalDate> expected = List.of(calendarUtil.create(2022, 10, 24),
+        calendarUtil.create(2022, 11, 2), calendarUtil.create(2022, 12, 25),
+        calendarUtil.create(2023, 1, 1), calendarUtil.create(2023, 1, 2),
+        calendarUtil.create(2023, 1, 3), calendarUtil.create(2023, 1, 22));
+      assertThat(holidays).hasSameSizeAs(expected);
+      for (LocalDate d : expected) {
+        assertThat(calendarUtil.contains(holidays, d)).isTrue();
+      }
+    } catch (Exception e) {
+      fail("Unexpected error occurred: " + e.getClass().getName() + " - " + e.getMessage());
+    }
+  }
+
+  @Test
+  void testManagerDifferentInstance() {
+    final Locale defaultLocale = Locale.getDefault();
+    Locale.setDefault(Locale.US);
+    try {
+      final HolidayManager defaultManager = HolidayManager.getInstance();
+      final HolidayManager virginIslandsManager = HolidayManager.getInstance(ManagerParameters.create(HolidayCalendar.MAURITIUS, null));
+      assertThat(defaultManager).isNotEqualTo(virginIslandsManager);
+    } catch (Exception e) {
+      fail("Unexpected error occurred: " + e.getClass().getName() + " - " + e.getMessage());
+    } finally {
+      Locale.setDefault(defaultLocale);
+    }
+  }
+}
