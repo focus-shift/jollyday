73d50bdd09cb0c340870d83ae3bf3b1b707f6a36
Tobias Schneider <Tobias.Schneider@Tobsch.org>
2023-09-22 10:58:35 +0200

    Add moving conditions to islamic holiday

--- jollyday-core/src/main/java/de/focus_shift/jollyday/core/spi/IslamicHoliday.java
+++ jollyday-core/src/main/java/de/focus_shift/jollyday/core/spi/IslamicHoliday.java
@@ -1,5 +1,5 @@
 package de.focus_shift.jollyday.core.spi;
 
-public interface IslamicHoliday extends Described, Limited {
+public interface IslamicHoliday extends Described, Limited, Movable {
   IslamicHolidayType type();
 }
