62e38352ec7575d5cb537b2b340cbbd5934fa26d
XSpielinbox <55600187+xspielinbox@users.noreply.github.com>
2023-09-27 19:52:52 +0200

    Add Isle of Man 2013 holiday test
    Signed-off-by: XSpielinbox <55600187+xspielinbox@users.noreply.github.com>

--- jollyday-tests/src/test/java/de/focus_shift/jollyday/tests/country/HolidayIMTest.java
+++ jollyday-tests/src/test/java/de/focus_shift/jollyday/tests/country/HolidayIMTest.java
@@ -43,7 +43,7 @@ class HolidayIMTest extends AbstractCountryTestBase {
   }
 
   @ParameterizedTest
-  @ValueSource(ints = {2010, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024})
+  @ValueSource(ints = {2010, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024})
   void testManagerIMStructure(final int year) {
     validateCalendarData(ISO_CODE, year, true);
   }
