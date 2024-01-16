9d8b240f48e841293a949ae80846e8fe1356fd66
Tobias Schneider <Tobias.Schneider@Tobsch.org>
2024-01-16 21:44:45 +0100

    Add javadoc to TestProvider

--- jollyday-core/src/test/java/de/focus_shift/jollyday/core/configuration/TestProvider.java
+++ jollyday-core/src/test/java/de/focus_shift/jollyday/core/configuration/TestProvider.java
@@ -2,6 +2,9 @@ package de.focus_shift.jollyday.core.configuration;
 
 import java.util.Properties;
 
+/**
+ * This provider is used in {@link ConfigurationProviderManagerTest}
+ */
 public class TestProvider implements ConfigurationProvider {
 
   private final Properties properties = new Properties();
