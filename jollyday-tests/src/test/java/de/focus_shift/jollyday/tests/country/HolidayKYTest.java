ba18489da6bbcabfa91eae76a17d28d7c9a539e8
XSpielinbox <55600187+xspielinbox@users.noreply.github.com>
2024-02-02 00:03:50 +0100

    Check holidays for KY
    Signed-off-by: XSpielinbox <55600187+xspielinbox@users.noreply.github.com>

--- jollyday-tests/src/test/java/de/focus_shift/jollyday/tests/country/HolidayKYTest.java
+++ jollyday-tests/src/test/java/de/focus_shift/jollyday/tests/country/HolidayKYTest.java
@@ -22,7 +22,7 @@ class HolidayKYTest extends AbstractCountryTestBase {
 
 
   @ParameterizedTest
-  @ValueSource(ints = {2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023})
+  @ValueSource(ints = {2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024})
   void testManagerKYStructure(final int year) {
     validateCalendarData(ISO_CODE, year, true);
   }
