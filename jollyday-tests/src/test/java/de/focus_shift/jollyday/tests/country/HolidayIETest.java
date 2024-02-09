34c89f20d26268c1ba9e3fdf70daffd4b4db5bf8
Tobias Schneider <Tobias.Schneider@Tobsch.org>
2024-02-09 09:31:15 +0100

    Add ireland tests for fixed holidays

---
+++ jollyday-tests/src/test/java/de/focus_shift/jollyday/tests/country/HolidayIETest.java
@@ -0,0 +1,78 @@
+package de.focus_shift.jollyday.tests.country;
+
+import de.focus_shift.jollyday.core.Holiday;
+import de.focus_shift.jollyday.core.HolidayManager;
+import net.jqwik.api.ForAll;
+import net.jqwik.api.Property;
+import net.jqwik.time.api.constraints.YearRange;
+
+import java.time.LocalDate;
+import java.time.Year;
+import java.util.Set;
+
+import static de.focus_shift.jollyday.core.HolidayCalendar.IRELAND;
+import static de.focus_shift.jollyday.core.HolidayType.OFFICIAL_HOLIDAY;
+import static de.focus_shift.jollyday.core.ManagerParameters.create;
+import static java.time.Month.DECEMBER;
+import static java.time.Month.JANUARY;
+import static java.time.Month.MARCH;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class HolidayIETest extends AbstractCountryTestBase {
+
+  @Property
+  void ensuresThatNewYearConfigured(@ForAll @YearRange Year year) {
+    final HolidayManager holidayManager = HolidayManager.getInstance(create(IRELAND));
+    final Set<Holiday> holidays = holidayManager.getHolidays(year.getValue());
+    assertThat(holidays)
+      .isNotEmpty()
+      .contains(new Holiday(LocalDate.of(year.getValue(), JANUARY, 1), "NEW_YEAR", OFFICIAL_HOLIDAY));
+  }
+
+  @Property
+  void ensuresThatStPatrickConfigured(@ForAll @YearRange Year year) {
+    final HolidayManager holidayManager = HolidayManager.getInstance(create(IRELAND));
+    final Set<Holiday> holidays = holidayManager.getHolidays(year.getValue());
+    assertThat(holidays)
+      .isNotEmpty()
+      .contains(new Holiday(LocalDate.of(year.getValue(), MARCH, 17), "ST_PATRICK", OFFICIAL_HOLIDAY));
+  }
+
+  @Property
+  void ensuresThatChristmasIsConfigured(@ForAll @YearRange Year year) {
+    final HolidayManager holidayManager = HolidayManager.getInstance(create(IRELAND));
+    final Set<Holiday> holidays = holidayManager.getHolidays(year.getValue());
+    assertThat(holidays)
+      .isNotEmpty()
+      .contains(new Holiday(LocalDate.of(year.getValue(), DECEMBER, 25), "CHRISTMAS", OFFICIAL_HOLIDAY));
+  }
+
+  @Property
+  void ensuresThatStephensIsConfigured(@ForAll @YearRange Year year) {
+    final HolidayManager holidayManager = HolidayManager.getInstance(create(IRELAND));
+    final Set<Holiday> holidays = holidayManager.getHolidays(year.getValue());
+    assertThat(holidays)
+      .isNotEmpty()
+      .contains(new Holiday(LocalDate.of(year.getValue(), DECEMBER, 26), "STEPHENS", OFFICIAL_HOLIDAY));
+  }
+
+  @Property
+  void ensuresThatEasterIsConfigured(@ForAll @YearRange Year year) {
+    final HolidayManager holidayManager = HolidayManager.getInstance(create(IRELAND));
+    final Set<Holiday> holidays = holidayManager.getHolidays(year.getValue());
+    assertThat(holidays)
+      .isNotEmpty()
+      .extracting(Holiday::getPropertiesKey)
+      .contains("christian.EASTER");
+  }
+
+  @Property
+  void ensuresThatEasterMondayIsConfigured(@ForAll @YearRange Year year) {
+    final HolidayManager holidayManager = HolidayManager.getInstance(create(IRELAND));
+    final Set<Holiday> holidays = holidayManager.getHolidays(year.getValue());
+    assertThat(holidays)
+      .isNotEmpty()
+      .extracting(Holiday::getPropertiesKey)
+      .contains("christian.EASTER_MONDAY");
+  }
+}
