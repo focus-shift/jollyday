38180eddadd601291865d253c04c7f2d1d4e6a91
Tobias Schneider <Tobias.Schneider@Tobsch.org>
2024-02-21 10:45:24 +0100

    Add new holiday types: bank, school, authorities

--- jollyday-core/src/main/java/de/focus_shift/jollyday/core/HolidayType.java
+++ jollyday-core/src/main/java/de/focus_shift/jollyday/core/HolidayType.java
@@ -2,7 +2,7 @@ package de.focus_shift.jollyday.core;
 
 /**
  * Type of holiday. Each holiday can be placed in a category and this is
- * represented by this type. The categories can mark a holiday as a official
+ * represented by this type. The categories can mark a holiday as an official
  * holiday or not.
  */
 public enum HolidayType {
@@ -14,6 +14,28 @@ public enum HolidayType {
     }
   },
 
+  BANK_HOLIDAY {
+    @Override
+    public boolean isOfficialHoliday() {
+      return false;
+    }
+  },
+
+  SCHOOL_HOLIDAY {
+    @Override
+    public boolean isOfficialHoliday() {
+      return false;
+    }
+  },
+
+  AUTHORITIES_HOLIDAY {
+    @Override
+    public boolean isOfficialHoliday() {
+      return false;
+    }
+  },
+
+  @Deprecated(since = "0.26.0", forRemoval = true)
   UNOFFICIAL_HOLIDAY {
     @Override
     public boolean isOfficialHoliday() {
