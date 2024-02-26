9ec9c0276d7518e646be2fc39a5d36c9e1b4395c
Tobias Schneider <Tobias.Schneider@Tobsch.org>
2024-02-26 09:58:48 +0100

    Add test for CalculateEasterSunday

---
+++ jollyday-core/src/test/java/de/focus_shift/jollyday/core/parser/functions/CalculateEasterSundayTest.java
@@ -0,0 +1,37 @@
+package de.focus_shift.jollyday.core.parser.functions;
+
+import org.junit.jupiter.api.Test;
+import org.threeten.extra.chrono.JulianChronology;
+
+import java.time.LocalDate;
+import java.time.chrono.IsoChronology;
+import java.time.chrono.MinguoChronology;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CalculateEasterSundayTest {
+
+  @Test
+  void ensureThatJulianEasterCalculationWasChosenByJulianChronology() {
+    final LocalDate julianEasterSunday = new CalculateEasterSunday(2005).apply(JulianChronology.INSTANCE);
+    assertThat(julianEasterSunday).isEqualTo(LocalDate.of(2005, 5, 1));
+  }
+
+  @Test
+  void ensureThatGregorianEasterCalculationWasChosenByGregorianChronology() {
+    final LocalDate gregorianEasterSunday = new CalculateEasterSunday(2005).apply(IsoChronology.INSTANCE);
+    assertThat(gregorianEasterSunday).isEqualTo(LocalDate.of(2005, 3, 27));
+  }
+
+  @Test
+  void ensureThatGregorianEasterCalculationWasChosenByUnknownChronologySince1584() {
+    final LocalDate gregorianEasterSunday = new CalculateEasterSunday(1584).apply(MinguoChronology.INSTANCE);
+    assertThat(gregorianEasterSunday).isEqualTo(LocalDate.of(1584, 4, 1));
+  }
+
+  @Test
+  void ensureThatJulianEasterCalculationWasChosenByUnknownChronologyUntil1583() {
+    final LocalDate julianEasterSunday = new CalculateEasterSunday(1583).apply(MinguoChronology.INSTANCE);
+    assertThat(julianEasterSunday).isEqualTo(LocalDate.of(1583, 4, 10));
+  }
+}
