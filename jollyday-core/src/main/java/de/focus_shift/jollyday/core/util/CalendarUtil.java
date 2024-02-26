3b73ddc43a0943d179f56192a232c1fab7faade9
Tobias Schneider <Tobias.Schneider@Tobsch.org>
2024-02-26 11:09:17 +0100

    Move test of CalendarUtil.getIslamicHolidaysInGregorianYear to CalculateRelativeDatesFromChronologyWithinGregorianYear
    ... the method CalendarUtil.getIslamicHolidaysInGregorianYear was never
    used. So we deleted it and moved the test to the generic function

--- jollyday-core/src/main/java/de/focus_shift/jollyday/core/util/CalendarUtil.java
+++ jollyday-core/src/main/java/de/focus_shift/jollyday/core/util/CalendarUtil.java
@@ -20,36 +20,6 @@ public class CalendarUtil {
   }
 
   /**
-   * Returns a set of gregorian dates within a gregorian year which equal the
-   * islamic month and day. Because the islamic year is about 11 days shorter
-   * than the gregorian there may be more than one occurrence of an islamic
-   * date in an gregorian year. i.e.: In the gregorian year 2008 there were
-   * two 1/1. They occurred on 1/10 and 12/29.
-   *
-   * @param gregorianYear a int.
-   * @param islamicMonth  a int.
-   * @param islamicDay    a int.
-   * @return List of gregorian dates for the islamic month/day.
-   */
-  public static Stream<LocalDate> getIslamicHolidaysInGregorianYear(int gregorianYear, int islamicMonth, int islamicDay) {
-    return getDatesFromChronologyWithinGregorianYear(islamicMonth, islamicDay, gregorianYear, HijrahChronology.INSTANCE);
-  }
-
-  /**
-   * Searches for the occurrences of a month/day in one chronology within one
-   * gregorian year.
-   *
-   * @param targetMonth
-   * @param targetDay
-   * @param gregorianYear
-   * @param targetChrono
-   * @return the list of gregorian dates.
-   */
-  private static Stream<LocalDate> getDatesFromChronologyWithinGregorianYear(int targetMonth, int targetDay, int gregorianYear, Chronology targetChrono) {
-    return new CalculateRelativeDatesFromChronologyWithinGregorianYear(targetMonth, targetDay, targetChrono, 0).apply(gregorianYear);
-  }
-
-  /**
    * Shows if the requested date is contained in the Set of holidays.
    * Calls #contains(holidays, date, null)
    *
