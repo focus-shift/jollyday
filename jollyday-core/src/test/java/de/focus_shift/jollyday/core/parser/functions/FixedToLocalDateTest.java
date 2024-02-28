89c5082ceca29a3acb90af4a913d3b0a3f7d43cf
Tobias Schneider <Tobias.Schneider@Tobsch.org>
2024-02-28 14:55:35 +0100

    Add FixedToLocalDateTest

---
+++ jollyday-core/src/test/java/de/focus_shift/jollyday/core/parser/functions/FixedToLocalDateTest.java
@@ -0,0 +1,65 @@
+package de.focus_shift.jollyday.core.parser.functions;
+
+import de.focus_shift.jollyday.core.HolidayType;
+import de.focus_shift.jollyday.core.spi.Fixed;
+import de.focus_shift.jollyday.core.spi.MovingCondition;
+import de.focus_shift.jollyday.core.spi.YearCycle;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.MonthDay;
+import java.time.Year;
+import java.util.List;
+
+import static java.time.Month.FEBRUARY;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class FixedToLocalDateTest {
+
+  @Test
+  void ensureToConvertFixedToLocalDate() {
+
+    final Fixed fixed = new Fixed() {
+      @Override
+      public List<MovingCondition> conditions() {
+        return null;
+      }
+
+      @Override
+      public Year validFrom() {
+        return null;
+      }
+
+      @Override
+      public Year validTo() {
+        return null;
+      }
+
+      @Override
+      public YearCycle cycle() {
+        return null;
+      }
+
+      @Override
+      public MonthDay day() {
+        return MonthDay.of(2, 28);
+      }
+
+      @Override
+      public String descriptionPropertiesKey() {
+        return null;
+      }
+
+      @Override
+      public HolidayType officiality() {
+        return null;
+      }
+    };
+
+    final LocalDate localDate = new FixedToLocalDate(2024).apply(fixed);
+    assertThat(localDate)
+      .hasYear(2024)
+      .hasMonth(FEBRUARY)
+      .hasDayOfMonth(28);
+  }
+}
