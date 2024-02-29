358796f5166ec0f86e3c0900cb47e9d7980b413c
Tobias Schneider <Tobias.Schneider@Tobsch.org>
2024-02-29 16:07:53 +0100

    Add benchmark tests via jmh

---
+++ jollyday-tests/src/test/java/de/focus_shift/jollyday/tests/HolidayManagerGetHolidayBenchmarkTest.java
@@ -0,0 +1,60 @@
+package de.focus_shift.jollyday.tests;
+
+import de.focus_shift.jollyday.core.Holiday;
+import de.focus_shift.jollyday.core.HolidayManager;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Warmup;
+import org.openjdk.jmh.results.RunResult;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import java.util.Collection;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+import static de.focus_shift.jollyday.core.ManagerParameters.create;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@BenchmarkMode(Mode.Throughput)
+@Fork(3)
+@Warmup(iterations = 5, time = 2000, timeUnit = TimeUnit.MILLISECONDS)
+@Measurement(iterations = 5, time = 2000, timeUnit = TimeUnit.MILLISECONDS)
+public class HolidayManagerGetHolidayBenchmarkTest extends Benchmarks {
+
+  private static final double REFERENCE_SCORE = 45_000.00;
+
+  @State(Scope.Thread)
+  public static class HolidayManagerState {
+    public final HolidayManager holidayManager = HolidayManager.getInstance(create("test"));
+  }
+
+  @Benchmark
+  public static Set<Holiday> benchmarkGetHolidays(final HolidayManagerState holidayManagerState) {
+    return holidayManagerState.holidayManager.getHolidays(2010);
+  }
+
+  @Test
+  @Tag("BenchmarkTest")
+  void runJmhBenchmark() throws RunnerException {
+    final Options opt = new OptionsBuilder()
+      .include(HolidayManagerGetHolidayBenchmarkTest.class.getSimpleName())
+      .build();
+
+    final Collection<RunResult> runResults = new Runner(opt).run();
+    assertThat(runResults).isNotEmpty();
+
+    for (RunResult runResult : runResults) {
+      assertDeviationWithin(runResult, REFERENCE_SCORE, 0.05);
+    }
+  }
+}
---
+++ jollyday-tests/src/test/java/de/focus_shift/jollyday/tests/HolidayManagerIsHolidayBenchmarkTest.java
@@ -0,0 +1,59 @@
+package de.focus_shift.jollyday.tests;
+
+import de.focus_shift.jollyday.core.HolidayManager;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Warmup;
+import org.openjdk.jmh.results.RunResult;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.concurrent.TimeUnit;
+
+import static de.focus_shift.jollyday.core.ManagerParameters.create;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@BenchmarkMode(Mode.Throughput)
+@Fork(3)
+@Warmup(iterations = 5, time = 2000, timeUnit = TimeUnit.MILLISECONDS)
+@Measurement(iterations = 5, time = 2000, timeUnit = TimeUnit.MILLISECONDS)
+public class HolidayManagerIsHolidayBenchmarkTest extends Benchmarks {
+
+  private static final double REFERENCE_SCORE = 8_300_000.000;
+
+  @State(Scope.Thread)
+  public static class HolidayManagerState {
+    public final HolidayManager holidayManager = HolidayManager.getInstance(create("test"));
+  }
+
+  @Benchmark
+  public static boolean benchmarkIsHoliday(final HolidayManagerState holidayManagerState) {
+    return holidayManagerState.holidayManager.isHoliday(LocalDate.of(2010, 1, 1));
+  }
+
+  @Test
+  @Tag("BenchmarkTest")
+  void runJmhBenchmark() throws RunnerException {
+    final Options opt = new OptionsBuilder()
+      .include(HolidayManagerIsHolidayBenchmarkTest.class.getSimpleName())
+      .build();
+
+    final Collection<RunResult> runResults = new Runner(opt).run();
+    assertThat(runResults).isNotEmpty();
+
+    for (RunResult runResult : runResults) {
+      assertDeviationWithin(runResult, REFERENCE_SCORE, 0.05);
+    }
+  }
+}
