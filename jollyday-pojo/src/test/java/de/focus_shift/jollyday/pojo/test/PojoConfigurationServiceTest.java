cdbcd078c0322a14204cd4b0783b460096d6f067
Tobias Schneider <Tobias.Schneider@Tobsch.org>
2024-12-24 11:24:49 +0100

    Fix to latest version

--- jollyday-pojo/src/test/java/de/focus_shift/jollyday/pojo/test/PojoConfigurationServiceTest.java
+++ jollyday-pojo/src/test/java/de/focus_shift/jollyday/pojo/test/PojoConfigurationServiceTest.java
@@ -7,13 +7,14 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import de.focus_shift.jollyday.core.HolidayManager;
-import de.focus_shift.jollyday.core.HolidayType;
 import de.focus_shift.jollyday.core.ManagerParameter;
 import de.focus_shift.jollyday.core.ManagerParameters;
 import de.focus_shift.jollyday.core.spi.Configuration;
 import de.focus_shift.jollyday.core.spi.YearCycle;
 import de.focus_shift.jollyday.pojo.PojoConfigurationService;
 import de.focus_shift.jollyday.pojo.PojoFixed;
+
+import static de.focus_shift.jollyday.core.HolidayType.OBSERVANCE;
 
 class PojoConfigurationServiceTest {
 
@@ -24,16 +25,16 @@ class PojoConfigurationServiceTest {
     ManagerParameter parameter = ManagerParameters.create("de");
 
     HolidayManager holidayManager = HolidayManager.getInstance(parameter);
-    Assertions.assertFalse(holidayManager.isHoliday(LocalDate.of(2022, 03, 22)), "Precondition 22.3 should be no holiday");
+    Assertions.assertFalse(holidayManager.isHoliday(LocalDate.of(2022, 3, 22)), "Precondition 22.3 should be no holiday");
 
     // add new holiday for 22.3 dynamically via code/api
     Configuration configuration = javaConfigurationService.getConfiguration(parameter);
-    PojoFixed stgandulfholiday = new PojoFixed("St.Gandulf's Day", HolidayType.UNOFFICIAL_HOLIDAY, null, null, YearCycle.EVERY_YEAR, null, MonthDay.of(3, 22));
+    PojoFixed stgandulfholiday = new PojoFixed("St.Gandulf's Day", OBSERVANCE, null, null, YearCycle.EVERY_YEAR, null, MonthDay.of(3, 22));
     configuration.holidays().fixed().add(stgandulfholiday);
-    HolidayManager.clearManagerCache(); // we have to clear the manager cache because otherwise the holidaymanager from above with old configuration would be reused.
+    HolidayManager.clearManagerCache(); // we have to clear the manager cache because otherwise the holiday manager from above with old configuration would be reused.
 
     holidayManager = HolidayManager.getInstance(parameter);
-    Assertions.assertTrue(holidayManager.isHoliday(LocalDate.of(2022, 03, 22), HolidayType.UNOFFICIAL_HOLIDAY), "Ensure newly added holiday is recognized");
+    Assertions.assertTrue(holidayManager.isHoliday(LocalDate.of(2022, 3, 22), OBSERVANCE), "Ensure newly added holiday is recognized");
 
   }
 }
