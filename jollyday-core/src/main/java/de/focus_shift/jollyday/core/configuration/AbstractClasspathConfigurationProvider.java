061718d23f9bc94eba4504b0f6667fd450f7f5a9
Tobias Schneider <Tobias.Schneider@Tobsch.org>
2025-01-13 23:16:03 +0100

    Save properties of AbstractClasspathConfigurationProvider in member variable to improve performance

--- jollyday-core/src/main/java/de/focus_shift/jollyday/core/configuration/AbstractClasspathConfigurationProvider.java
+++ jollyday-core/src/main/java/de/focus_shift/jollyday/core/configuration/AbstractClasspathConfigurationProvider.java
@@ -10,34 +10,40 @@ import java.util.Properties;
 
 abstract class AbstractClasspathConfigurationProvider implements ConfigurationProvider {
 
-    static final String DEFAULT_CONFIGURATION_FILE_NAME = "jollyday.properties";
+  static final String DEFAULT_CONFIGURATION_FILE_NAME = "jollyday.properties";
 
-    private final URL configurationFile;
+  private final Properties properties;
 
-    AbstractClasspathConfigurationProvider() {
-        this(false);
+  AbstractClasspathConfigurationProvider() {
+    this(false);
+  }
+
+  AbstractClasspathConfigurationProvider(final boolean searchOnlyInJar) {
+    this.properties = loadProperties(searchOnlyInJar);
+  }
+
+  @Override
+  public Properties getProperties() {
+    return properties;
+  }
+
+  private Properties loadProperties(final boolean searchOnlyInJar) {
+    return getConfigurationFile(searchOnlyInJar)
+      .map(this::toProperties)
+      .orElseGet(Properties::new);
+  }
+
+  private Properties toProperties(final URL propertiesFile) {
+    final Properties props = new Properties();
+    try (final InputStream inputStream = propertiesFile.openStream()) {
+      props.load(inputStream);
+    } catch (IOException e) {
+      throw new IllegalStateException("Could not load default configuration from classpath.", e);
     }
+    return props;
+  }
 
-    AbstractClasspathConfigurationProvider(final boolean searchOnlyInJar) {
-        this.configurationFile = getConfigurationFile(searchOnlyInJar).orElse(null);
-    }
-
-    @Override
-    public Properties getProperties() {
-        final Properties properties = new Properties();
-
-        if (configurationFile != null) {
-            try (final InputStream inputStream = configurationFile.openStream()) {
-                properties.load(inputStream);
-            } catch (IOException e) {
-                throw new IllegalStateException("Could not load default configuration from classpath.", e);
-            }
-        }
-
-        return properties;
-    }
-
-    private Optional<URL> getConfigurationFile(final boolean searchOnlyInJar) {
-        return ResourceUtil.getResource(DEFAULT_CONFIGURATION_FILE_NAME, searchOnlyInJar);
-    }
+  private Optional<URL> getConfigurationFile(final boolean searchOnlyInJar) {
+    return ResourceUtil.getResource(DEFAULT_CONFIGURATION_FILE_NAME, searchOnlyInJar);
+  }
 }
