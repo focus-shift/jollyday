56cded085659ee6c340aa4f35ae425cdbee154a4
Tobias Schneider <Tobias.Schneider@Tobsch.org>
2025-06-05 13:39:46 +0200

    Use CalendarChecker for san marino tests

--- jollyday-tests/src/test/java/de/focus_shift/jollyday/tests/country/HolidaySMTest.java
+++ jollyday-tests/src/test/java/de/focus_shift/jollyday/tests/country/HolidaySMTest.java
@@ -1,18 +1,9 @@
 package de.focus_shift.jollyday.tests.country;
 
-import de.focus_shift.jollyday.core.Holiday;
-import de.focus_shift.jollyday.core.HolidayManager;
-import net.jqwik.api.ForAll;
-import net.jqwik.api.Property;
-import net.jqwik.time.api.constraints.YearRange;
-
-import java.time.LocalDate;
-import java.time.Year;
-import java.util.Set;
+import org.junit.jupiter.api.Test;
 
 import static de.focus_shift.jollyday.core.HolidayCalendar.SAN_MARINO;
-import static de.focus_shift.jollyday.core.HolidayType.PUBLIC_HOLIDAY;
-import static de.focus_shift.jollyday.core.ManagerParameters.create;
+import static de.focus_shift.jollyday.tests.CalendarCheckerApi.assertFor;
 import static java.time.Month.AUGUST;
 import static java.time.Month.DECEMBER;
 import static java.time.Month.FEBRUARY;
@@ -22,147 +13,30 @@ import static java.time.Month.MARCH;
 import static java.time.Month.MAY;
 import static java.time.Month.NOVEMBER;
 import static java.time.Month.SEPTEMBER;
-import static org.assertj.core.api.Assertions.assertThat;
 
 class HolidaySMTest {
 
-  @Property
-  void ensuresThatNewYearIsOnFirstJanuary(@ForAll @YearRange Year year) {
-    final HolidayManager holidayManager = HolidayManager.getInstance(create(SAN_MARINO));
-    final Set<Holiday> holidays = holidayManager.getHolidays(year);
-    assertThat(holidays)
-      .isNotEmpty()
-      .contains(new Holiday(LocalDate.of(year.getValue(), JANUARY, 1), "NEW_YEAR", PUBLIC_HOLIDAY));
-  }
-
-  @Property
-  void ensuresThatEpiphanyConfigured(@ForAll @YearRange Year year) {
-    final HolidayManager holidayManager = HolidayManager.getInstance(create(SAN_MARINO));
-    final Set<Holiday> holidays = holidayManager.getHolidays(year);
-    assertThat(holidays)
-      .isNotEmpty()
-      .contains(new Holiday(LocalDate.of(year.getValue(), JANUARY, 6), "EPIPHANY", PUBLIC_HOLIDAY));
-  }
-
-  @Property
-  void ensuresThatFeastOfSaintAgathaIsConfigured(@ForAll @YearRange Year year) {
-    final HolidayManager holidayManager = HolidayManager.getInstance(create(SAN_MARINO));
-    final Set<Holiday> holidays = holidayManager.getHolidays(year);
-    assertThat(holidays)
-      .isNotEmpty()
-      .contains(new Holiday(LocalDate.of(year.getValue(), FEBRUARY, 5), "FEAST_OF_SAINT_AGATHA", PUBLIC_HOLIDAY));
-  }
-
-  @Property
-  void ensuresThatAnniversaryOfTheArengoIsConfigured(@ForAll @YearRange Year year) {
-    final HolidayManager holidayManager = HolidayManager.getInstance(create(SAN_MARINO));
-    final Set<Holiday> holidays = holidayManager.getHolidays(year);
-    assertThat(holidays)
-      .isNotEmpty()
-      .contains(new Holiday(LocalDate.of(year.getValue(), MARCH, 25), "ANNIVERSARY_OF_THE_ARENGO", PUBLIC_HOLIDAY));
-  }
-
-  @Property
-  void ensuresThatLabourDayIsConfigured(@ForAll @YearRange Year year) {
-    final HolidayManager holidayManager = HolidayManager.getInstance(create(SAN_MARINO));
-    final Set<Holiday> holidays = holidayManager.getHolidays(year);
-    assertThat(holidays)
-      .isNotEmpty()
-      .contains(new Holiday(LocalDate.of(year.getValue(), MAY, 1), "LABOUR_DAY", PUBLIC_HOLIDAY));
-  }
-
-  @Property
-  void ensuresThatLiberationFromFascismIsConfigured(@ForAll @YearRange Year year) {
-    final HolidayManager holidayManager = HolidayManager.getInstance(create(SAN_MARINO));
-    final Set<Holiday> holidays = holidayManager.getHolidays(year);
-    assertThat(holidays)
-      .isNotEmpty()
-      .contains(new Holiday(LocalDate.of(year.getValue(), JULY, 28), "LIBERATION_FROM_FASCISM", PUBLIC_HOLIDAY));
-  }
-
-  @Property
-  void ensuresThatAssumptionDayIsConfigured(@ForAll @YearRange Year year) {
-    final HolidayManager holidayManager = HolidayManager.getInstance(create(SAN_MARINO));
-    final Set<Holiday> holidays = holidayManager.getHolidays(year);
-    assertThat(holidays)
-      .isNotEmpty()
-      .contains(new Holiday(LocalDate.of(year.getValue(), AUGUST, 15), "ASSUMPTION_DAY", PUBLIC_HOLIDAY));
-  }
-
-  @Property
-  void ensuresThatTheFeastOfStMarinusAndRepublicDayIsConfigured(@ForAll @YearRange Year year) {
-    final HolidayManager holidayManager = HolidayManager.getInstance(create(SAN_MARINO));
-    final Set<Holiday> holidays = holidayManager.getHolidays(year);
-    assertThat(holidays)
-      .isNotEmpty()
-      .contains(new Holiday(LocalDate.of(year.getValue(), SEPTEMBER, 3), "THE_FEAST_OF_SAINT_MARINUS_AND_REPUBLIC_DAY", PUBLIC_HOLIDAY));
-  }
-
-  @Property
-  void ensuresThatAllSaintsIsConfigured(@ForAll @YearRange Year year) {
-    final HolidayManager holidayManager = HolidayManager.getInstance(create(SAN_MARINO));
-    final Set<Holiday> holidays = holidayManager.getHolidays(year);
-    assertThat(holidays)
-      .isNotEmpty()
-      .contains(new Holiday(LocalDate.of(year.getValue(), NOVEMBER, 1), "ALL_SAINTS", PUBLIC_HOLIDAY));
-  }
-
-  @Property
-  void ensuresThatAllSoulsIsConfigured(@ForAll @YearRange Year year) {
-    final HolidayManager holidayManager = HolidayManager.getInstance(create(SAN_MARINO));
-    final Set<Holiday> holidays = holidayManager.getHolidays(year);
-    assertThat(holidays)
-      .isNotEmpty()
-      .contains(new Holiday(LocalDate.of(year.getValue(), NOVEMBER, 2), "ALL_SOULS", PUBLIC_HOLIDAY));
-  }
-
-  @Property
-  void ensuresThatImmaculateConceptionIsConfigured(@ForAll @YearRange Year year) {
-    final HolidayManager holidayManager = HolidayManager.getInstance(create(SAN_MARINO));
-    final Set<Holiday> holidays = holidayManager.getHolidays(year);
-    assertThat(holidays)
-      .isNotEmpty()
-      .contains(new Holiday(LocalDate.of(year.getValue(), DECEMBER, 8), "IMMACULATE_CONCEPTION", PUBLIC_HOLIDAY));
-  }
-
-
-  @Property
-  void ensuresThatChristmasIsConfigured(@ForAll @YearRange Year year) {
-    final HolidayManager holidayManager = HolidayManager.getInstance(create(SAN_MARINO));
-    final Set<Holiday> holidays = holidayManager.getHolidays(year);
-    assertThat(holidays)
-      .isNotEmpty()
-      .contains(new Holiday(LocalDate.of(year.getValue(), DECEMBER, 24), "CHRISTMAS_EVE", PUBLIC_HOLIDAY))
-      .contains(new Holiday(LocalDate.of(year.getValue(), DECEMBER, 25), "CHRISTMAS", PUBLIC_HOLIDAY))
-      .contains(new Holiday(LocalDate.of(year.getValue(), DECEMBER, 26), "STEPHENS", PUBLIC_HOLIDAY));
-  }
-
-  @Property
-  void ensuresThatNewYearsEveIsConfigured(@ForAll @YearRange Year year) {
-    final HolidayManager holidayManager = HolidayManager.getInstance(create(SAN_MARINO));
-    final Set<Holiday> holidays = holidayManager.getHolidays(year);
-    assertThat(holidays)
-      .isNotEmpty()
-      .contains(new Holiday(LocalDate.of(year.getValue(), DECEMBER, 31), "NEW_YEARS_EVE", PUBLIC_HOLIDAY));
-  }
-
-  @Property
-  void ensuresThatEasterMondayIsConfigured(@ForAll @YearRange Year year) {
-    final HolidayManager holidayManager = HolidayManager.getInstance(create(SAN_MARINO));
-    final Set<Holiday> holidays = holidayManager.getHolidays(year);
-    assertThat(holidays)
-      .isNotEmpty()
-      .extracting(Holiday::getPropertiesKey)
-      .contains("christian.EASTER_MONDAY");
-  }
-
-  @Property
-  void ensuresThatCorpusChristiIsConfigured(@ForAll @YearRange Year year) {
-    final HolidayManager holidayManager = HolidayManager.getInstance(create(SAN_MARINO));
-    final Set<Holiday> holidays = holidayManager.getHolidays(year);
-    assertThat(holidays)
-      .isNotEmpty()
-      .extracting(Holiday::getPropertiesKey)
-      .contains("christian.CORPUS_CHRISTI");
+  @Test
+  void ensuresSanMarinoHolidays() {
+    assertFor(SAN_MARINO)
+      .hasFixedHoliday("NEW_YEAR", JANUARY, 1).and()
+      .hasFixedHoliday("EPIPHANY", JANUARY, 6).and()
+      .hasFixedHoliday("FEAST_OF_SAINT_AGATHA", FEBRUARY, 5).and()
+      .hasFixedHoliday("ANNIVERSARY_OF_THE_ARENGO", MARCH, 25).and()
+      .hasFixedHoliday("LABOUR_DAY", MAY, 1).and()
+      .hasFixedHoliday("LIBERATION_FROM_FASCISM", JULY, 28).and()
+      .hasFixedHoliday("ASSUMPTION_DAY", AUGUST, 15).and()
+      .hasFixedHoliday("THE_FEAST_OF_SAINT_MARINUS_AND_REPUBLIC_DAY", SEPTEMBER, 3).and()
+      .hasFixedHoliday("ALL_SAINTS", NOVEMBER, 1).and()
+      .hasFixedHoliday("ALL_SOULS", NOVEMBER, 2).and()
+      .hasFixedHoliday("IMMACULATE_CONCEPTION", DECEMBER, 8).and()
+      .hasFixedHoliday("CHRISTMAS_EVE", DECEMBER, 24).and()
+      .hasFixedHoliday("CHRISTMAS", DECEMBER, 25).and()
+      .hasFixedHoliday("STEPHENS", DECEMBER, 26).and()
+      .hasFixedHoliday("NEW_YEARS_EVE", DECEMBER, 31).and()
+      .hasChristianHoliday("EASTER").and()
+      .hasChristianHoliday("EASTER_MONDAY").and()
+      .hasChristianHoliday("CORPUS_CHRISTI")
+      .check();
   }
 }
