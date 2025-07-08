5ee768ce11d87faa2832c224c53e371eeae43020
Tobias Schneider <Tobias.Schneider@Tobsch.org>
2025-07-08 09:40:06 +0200

    relative to fixed parser: improve readability

--- jollyday-core/src/main/java/de/focus_shift/jollyday/core/parser/impl/RelativeToFixedParser.java
+++ jollyday-core/src/main/java/de/focus_shift/jollyday/core/parser/impl/RelativeToFixedParser.java
@@ -7,12 +7,14 @@ import de.focus_shift.jollyday.core.parser.functions.FixedToLocalDate;
 import de.focus_shift.jollyday.core.parser.predicates.ValidLimitation;
 import de.focus_shift.jollyday.core.spi.Holidays;
 import de.focus_shift.jollyday.core.spi.Relation;
+import org.threeten.extra.Days;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.Year;
 import java.util.List;
 
+import static de.focus_shift.jollyday.core.spi.Relation.BEFORE;
 import static java.util.stream.Collectors.toList;
 
 /**
@@ -27,21 +29,35 @@ public class RelativeToFixedParser implements HolidayParser {
       .map(rf -> {
         LocalDate fixed = new FixedToLocalDate(year).apply(rf.date());
         if (rf.weekday() != null) {
-          // if weekday is set -> move to weekday
-          final DayOfWeek day = rf.weekday();
-          final int direction = rf.when() == Relation.BEFORE ? -1 : 1;
-          // don't test start day
-
-          do {
-            fixed = fixed.plusDays(direction);
-          } while (fixed.getDayOfWeek() != day);
-
+          fixed = moveToWeekday(fixed, rf.weekday(), rf.when());
         } else if (rf.days() != null) {
-          // if number of days set -> move number of days
-          fixed = fixed.plus(rf.when() == Relation.BEFORE ? rf.days().negated() : rf.days());
+          fixed = moveByDays(fixed, rf.days(), rf.when());
         }
         return new CreateHoliday(fixed).apply(rf);
       })
       .collect(toList());
+  }
+
+  private LocalDate moveToWeekday(LocalDate date, DayOfWeek targetDay, Relation relation) {
+
+    final int direction = (relation == BEFORE ? -1 : 1);
+    final int currentDayValue = date.getDayOfWeek().getValue();
+    final int targetDayValue = targetDay.getValue();
+
+    int daysDifference = targetDayValue - currentDayValue;
+    if (direction < 0 && daysDifference >= 0) {
+      daysDifference -= 7;
+    } else if (direction > 0 && daysDifference <= 0) {
+      daysDifference += 7;
+    }
+
+    return date.plusDays(daysDifference);
+  }
+
+  /**
+   * Moves the given date by the specified number of days in the given direction.
+   */
+  private LocalDate moveByDays(LocalDate date, Days days, Relation relation) {
+    return relation == BEFORE ? date.minus(days) : date.plus(days);
   }
 }
