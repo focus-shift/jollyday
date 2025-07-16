6eb4ebc56edddfcf48386722a0f9f006f837470b
Tobias Schneider <Tobias.Schneider@Tobsch.org>
2025-07-16 14:55:18 +0200

    use records
    closes #712
    17

--- jollyday-tests/src/test/java/de/focus_shift/jollyday/tests/CalendarChecker.java
+++ jollyday-tests/src/test/java/de/focus_shift/jollyday/tests/CalendarChecker.java
@@ -202,7 +202,7 @@ public class CalendarChecker implements
   public void check() {
     and();
 
-    final HolidayManager holidayManager = HolidayManager.getInstance(create(checks.get(0).getCalendar()));
+    final HolidayManager holidayManager = HolidayManager.getInstance(create(checks.get(0).calendar()));
 
     for (HolidayCalendarCheck check : checks) {
       switch (check.category) {
@@ -223,28 +223,28 @@ public class CalendarChecker implements
   private void checkByDate(final HolidayCalendarCheck check, final HolidayManager holidayManager) {
     final YearArbitrary yearArbitrary = createYearArbitrary();
 
-    for (final YearRange invalidRange : check.getInvalidRanges()) {
+    for (final YearRange invalidRange : check.invalidRanges()) {
       yearArbitrary
-        .between(invalidRange.getFrom().getValue(), invalidRange.getTo().getValue())
+        .between(invalidRange.from().getValue(), invalidRange.to().getValue())
         .forEachValue(year -> {
-            final Set<Holiday> holidays = holidayManager.getHolidays(year, check.getSubdivisions());
-            final LocalDate dateToCheck = LocalDate.of(year.getValue(), check.getMonth(), check.getDay());
+            final Set<Holiday> holidays = holidayManager.getHolidays(year, check.subdivisions());
+            final LocalDate dateToCheck = LocalDate.of(year.getValue(), check.month(), check.day());
             final LocalDate shiftLocalDate = shiftLocalDate(check, dateToCheck);
-            final Holiday holiday = new Holiday(shiftLocalDate, check.getPropertiesKey(), check.getHolidayType());
-            assertHolidayNotPresent(holidays, holiday, holidayManager, check.getSubdivisions());
+            final Holiday holiday = new Holiday(shiftLocalDate, check.propertiesKey(), check.holidayType());
+            assertHolidayNotPresent(holidays, holiday, holidayManager, check.subdivisions());
           }
         );
     }
 
-    for (final YearRange validRange : check.getValidRanges()) {
+    for (final YearRange validRange : check.validRanges()) {
       yearArbitrary
-        .between(validRange.getFrom().getValue(), validRange.getTo().getValue())
+        .between(validRange.from().getValue(), validRange.to().getValue())
         .forEachValue(year -> {
-            final Set<Holiday> holidays = holidayManager.getHolidays(year, check.getSubdivisions());
-            final LocalDate dateToCheck = LocalDate.of(year.getValue(), check.getMonth(), check.getDay());
+            final Set<Holiday> holidays = holidayManager.getHolidays(year, check.subdivisions());
+            final LocalDate dateToCheck = LocalDate.of(year.getValue(), check.month(), check.day());
             final LocalDate shiftLocalDate = shiftLocalDate(check, dateToCheck);
-            final Holiday holiday = new Holiday(shiftLocalDate, check.getPropertiesKey(), check.getHolidayType());
-            assertHolidayPresent(holidays, holiday, holidayManager, check.getSubdivisions());
+            final Holiday holiday = new Holiday(shiftLocalDate, check.propertiesKey(), check.holidayType());
+            assertHolidayPresent(holidays, holiday, holidayManager, check.subdivisions());
           }
         );
     }
@@ -277,11 +277,11 @@ public class CalendarChecker implements
 
     LocalDate shiftedDate = date;
     for (WeekDayFromTo shift : check.validShifts) {
-      if (shift != null && date.getDayOfWeek().equals(shift.getFrom())) {
+      if (shift != null && date.getDayOfWeek().equals(shift.from())) {
         if (shift.adjuster == NEXT) {
-          shiftedDate = date.with(nextOrSame(shift.getTo()));
+          shiftedDate = date.with(nextOrSame(shift.to()));
         } else {
-          shiftedDate = date.with(previousOrSame(shift.getTo()));
+          shiftedDate = date.with(previousOrSame(shift.to()));
         }
       }
     }
@@ -292,32 +292,32 @@ public class CalendarChecker implements
   private void checkByKey(final HolidayCalendarCheck check, final HolidayManager holidayManager) {
     final YearArbitrary yearArbitrary = createYearArbitrary();
 
-    for (final YearRange invalidRange : check.getInvalidRanges()) {
+    for (final YearRange invalidRange : check.invalidRanges()) {
       yearArbitrary
-        .between(invalidRange.getFrom().getValue(), invalidRange.getTo().getValue())
+        .between(invalidRange.from().getValue(), invalidRange.to().getValue())
         .forEachValue(year -> {
-            final Set<Holiday> holidays = holidayManager.getHolidays(year, check.getSubdivisions());
+            final Set<Holiday> holidays = holidayManager.getHolidays(year, check.subdivisions());
             assertThat(holidays)
               .isNotEmpty()
-              .filteredOn(holiday -> holiday.getPropertiesKey().equals(check.getPropertiesKey()))
+              .filteredOn(holiday -> holiday.getPropertiesKey().equals(check.propertiesKey()))
               .extracting(Holiday::getType)
-              .withFailMessage("Holiday '" + check.getPropertiesKey() + "' with holiday type '" + check.holidayType + "' in year '" + year + "' not found.")
-              .doesNotContain(check.getHolidayType());
+              .withFailMessage("Holiday '" + check.propertiesKey() + "' with holiday type '" + check.holidayType + "' in year '" + year + "' not found.")
+              .doesNotContain(check.holidayType());
           }
         );
     }
 
-    for (final YearRange validRange : check.getValidRanges()) {
+    for (final YearRange validRange : check.validRanges()) {
       yearArbitrary
-        .between(validRange.getFrom().getValue(), validRange.getTo().getValue())
+        .between(validRange.from().getValue(), validRange.to().getValue())
         .forEachValue(year -> {
-            final Set<Holiday> holidays = holidayManager.getHolidays(year, check.getSubdivisions());
+            final Set<Holiday> holidays = holidayManager.getHolidays(year, check.subdivisions());
             assertThat(holidays)
               .isNotEmpty()
-              .filteredOn(holiday -> holiday.getPropertiesKey().equals(check.getPropertiesKey()))
+              .filteredOn(holiday -> holiday.getPropertiesKey().equals(check.propertiesKey()))
               .extracting(Holiday::getType)
-              .withFailMessage("Holiday '" + check.getPropertiesKey() + "' with holiday type '" + check.holidayType + "' in year '" + year + "' and in subdivision '" + Arrays.toString(check.getSubdivisions()) + "' not found.")
-              .contains(check.getHolidayType());
+              .withFailMessage("Holiday '" + check.propertiesKey() + "' with holiday type '" + check.holidayType + "' in year '" + year + "' and in subdivision '" + Arrays.toString(check.subdivisions()) + "' not found.")
+              .contains(check.holidayType());
           }
         );
     }
@@ -339,20 +339,14 @@ public class CalendarChecker implements
     this.validShifts = new ArrayList<>();
   }
 
-  private static final class HolidayCalendarCheck {
+  private record HolidayCalendarCheck(
+    HolidayCalendar calendar, String propertiesKey, Month month, int day,
+    HolidayType holidayType, List<YearRange> validRanges,
+    List<YearRange> invalidRanges, List<WeekDayFromTo> validShifts,
+    String[] subdivisions, Category category
+  ) {
 
     private static final YearRange DEFAULT_YEAR_RANGE = new YearRange(Year.of(1900), Year.of(2500));
-
-    private final HolidayCalendar calendar;
-    private final List<YearRange> validRanges;
-    private final List<YearRange> invalidRanges;
-    private final List<WeekDayFromTo> validShifts;
-    private final Month month;
-    private final int day;
-    private final String propertiesKey;
-    private final HolidayType holidayType;
-    private final String[] subdivisions;
-    private final Category category;
 
     private HolidayCalendarCheck(
       HolidayCalendar calendar,
@@ -371,92 +365,16 @@ public class CalendarChecker implements
       this.subdivisions = subdivisions;
       this.category = category;
     }
-
-    public HolidayCalendar getCalendar() {
-      return calendar;
-    }
-
-    public List<YearRange> getValidRanges() {
-      return validRanges;
-    }
-
-    public List<YearRange> getInvalidRanges() {
-      return invalidRanges;
-    }
-
-    public List<WeekDayFromTo> getValidShifts() {
-      return validShifts;
-    }
-
-    public Month getMonth() {
-      return month;
-    }
-
-    public int getDay() {
-      return day;
-    }
-
-    public String getPropertiesKey() {
-      return propertiesKey;
-    }
-
-    public HolidayType getHolidayType() {
-      return holidayType;
-    }
-
-    public String[] getSubdivisions() {
-      return subdivisions;
-    }
-
-    public Category getCategory() {
-      return category;
-    }
   }
 
-  private static final class YearRange {
-
-    private final Year from;
-    private final Year to;
-
-    private YearRange(final Year from, final Year to) {
+  private record YearRange(Year from, Year to) {
+    private YearRange {
       if (from != null && to != null) {
         Assertions.assertFalse(from.isAfter(to), "To must be greater than or equal to the from year.");
       }
-      this.from = from;
-      this.to = to;
-    }
-
-    public Year getFrom() {
-      return from;
-    }
-
-    public Year getTo() {
-      return to;
     }
   }
 
-  private static final class WeekDayFromTo {
-
-    private final DayOfWeek from;
-    private final DayOfWeek to;
-    private final Adjuster adjuster;
-
-    private WeekDayFromTo(final DayOfWeek from, final DayOfWeek to, final Adjuster adjuster) {
-      this.from = from;
-      this.to = to;
-      this.adjuster = adjuster;
-    }
-
-    public DayOfWeek getFrom() {
-      return from;
-    }
-
-    public DayOfWeek getTo() {
-      return to;
-    }
-
-    public Adjuster getAdjuster() {
-      return adjuster;
-    }
+  private record WeekDayFromTo(DayOfWeek from, DayOfWeek to, Adjuster adjuster) {
   }
 }
