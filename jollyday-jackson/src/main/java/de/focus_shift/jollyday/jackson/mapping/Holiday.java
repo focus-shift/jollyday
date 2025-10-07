a741c49ebad82eb25b39e2ebbd11d05207b7d45c
Tobias Schneider <Tobias.Schneider@Tobsch.org>
2025-10-07 11:55:52 +0200

    Use Objects.requireNonNullElse

--- jollyday-jackson/src/main/java/de/focus_shift/jollyday/jackson/mapping/Holiday.java
+++ jollyday-jackson/src/main/java/de/focus_shift/jollyday/jackson/mapping/Holiday.java
@@ -2,6 +2,8 @@ package de.focus_shift.jollyday.jackson.mapping;
 
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
 
+import java.util.Objects;
+
 public abstract class Holiday {
 
   @JacksonXmlProperty(localName = "validFrom", isAttribute = true)
@@ -106,11 +108,7 @@ public abstract class Holiday {
    * {@link HolidayType }
    */
   public HolidayType getLocalizedType() {
-    if (localizedType == null) {
-      return HolidayType.PUBLIC_HOLIDAY;
-    } else {
-      return localizedType;
-    }
+    return Objects.requireNonNullElse(localizedType, HolidayType.PUBLIC_HOLIDAY);
   }
 
   /**
