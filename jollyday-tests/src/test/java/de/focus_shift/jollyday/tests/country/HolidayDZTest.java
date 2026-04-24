35f8a7d0b0b211a94d4ee6ad7862c878afdea258
Tobias Schneider <Tobias.Schneider@Tobsch.org>
2026-04-24 09:06:32 +0200

    algeria: add holidays

---
+++ jollyday-tests/src/test/java/de/focus_shift/jollyday/tests/country/HolidayDZTest.java
@@ -0,0 +1,34 @@
+package de.focus_shift.jollyday.tests.country;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.Year;
+
+import static de.focus_shift.jollyday.core.HolidayCalendar.ALGERIA;
+import static de.focus_shift.jollyday.tests.CalendarCheckerApi.assertFor;
+import static java.time.Month.JANUARY;
+import static java.time.Month.MAY;
+import static java.time.Month.JULY;
+import static java.time.Month.NOVEMBER;
+
+class HolidayDZTest {
+
+  private static final Year YEAR_FROM = Year.of(1900);
+  private static final Year YEAR_TO = Year.of(2173);
+
+  @Test
+  void ensuresHolidays() {
+    assertFor(ALGERIA)
+      .hasFixedHoliday("NEW_YEAR", JANUARY, 1).validBetween(YEAR_FROM, YEAR_TO).and()
+      .hasFixedHoliday("AMAZIGH_NEW_YEAR", JANUARY, 12).validBetween(YEAR_FROM, YEAR_TO).and()
+      .hasFixedHoliday("LABOUR_DAY", MAY, 1).validBetween(YEAR_FROM, YEAR_TO).and()
+      .hasFixedHoliday("INDEPENDENCE_DAY", JULY, 5).validBetween(Year.of(1962), YEAR_TO).and()
+      .hasFixedHoliday("REVOLUTION", NOVEMBER, 1).validBetween(Year.of(1954), YEAR_TO).and()
+      .hasIslamicHoliday("NEWYEAR").validBetween(YEAR_FROM, YEAR_TO).and()
+      .hasIslamicHoliday("ASCHURA").validBetween(YEAR_FROM, YEAR_TO).and()
+      .hasIslamicHoliday("MAWLID_AN_NABI").validBetween(YEAR_FROM, YEAR_TO).and()
+      .hasIslamicHoliday("ID_AL_FITR").validBetween(YEAR_FROM, YEAR_TO).and()
+      .hasIslamicHoliday("ID_UL_ADHA").validBetween(YEAR_FROM, YEAR_TO)
+      .check();
+  }
+}
