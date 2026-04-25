ed30db89bd6e0d1e135e3b91912b1fb8aa7030ca
Tobias Schneider <Tobias.Schneider@Tobsch.org>
2026-04-25 14:51:23 +0200

    chad: add holidays

---
+++ jollyday-tests/src/test/java/de/focus_shift/jollyday/tests/country/HolidayTDTest.java
@@ -0,0 +1,38 @@
+package de.focus_shift.jollyday.tests.country;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.Year;
+
+import static de.focus_shift.jollyday.core.HolidayCalendar.CHAD;
+import static de.focus_shift.jollyday.tests.CalendarCheckerApi.assertFor;
+import static java.time.Month.JANUARY;
+import static java.time.Month.MARCH;
+import static java.time.Month.MAY;
+import static java.time.Month.AUGUST;
+import static java.time.Month.NOVEMBER;
+import static java.time.Month.DECEMBER;
+
+class HolidayTDTest {
+
+  private static final Year YEAR_FROM = Year.of(1900);
+  private static final Year YEAR_TO = Year.of(2173);
+
+  @Test
+  void ensuresHolidays() {
+    assertFor(CHAD)
+      .hasFixedHoliday("NEW_YEAR", JANUARY, 1).validBetween(YEAR_FROM, YEAR_TO).and()
+      .hasFixedHoliday("WOMENS_DAY", MARCH, 8).validBetween(YEAR_FROM, YEAR_TO).and()
+      .hasFixedHoliday("LABOUR_DAY", MAY, 1).validBetween(YEAR_FROM, YEAR_TO).and()
+      .hasFixedHoliday("INDEPENDENCE_DAY", AUGUST, 11).validBetween(YEAR_FROM, YEAR_TO).and()
+      .hasFixedHoliday("ALL_SAINTS", NOVEMBER, 1).validBetween(YEAR_FROM, YEAR_TO).and()
+      .hasFixedHoliday("REPUBLIC_DAY", NOVEMBER, 28).validBetween(YEAR_FROM, YEAR_TO).and()
+      .hasFixedHoliday("FREEDOM_DEMOCRACY", DECEMBER, 1).validBetween(YEAR_FROM, YEAR_TO).and()
+      .hasFixedHoliday("CHRISTMAS", DECEMBER, 25).validBetween(YEAR_FROM, YEAR_TO).and()
+      .hasChristianHoliday("EASTER_MONDAY").validBetween(YEAR_FROM, YEAR_TO).and()
+      .hasIslamicHoliday("MAWLID_AN_NABI").validBetween(YEAR_FROM, YEAR_TO).and()
+      .hasIslamicHoliday("ID_AL_FITR").validBetween(YEAR_FROM, YEAR_TO).and()
+      .hasIslamicHoliday("ID_UL_ADHA").validBetween(YEAR_FROM, YEAR_TO)
+      .check();
+  }
+}
