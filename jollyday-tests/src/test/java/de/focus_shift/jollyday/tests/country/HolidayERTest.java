2b171166c77114afc16e296028de4a307e1c21ee
Tobias Schneider <Tobias.Schneider@Tobsch.org>
2026-05-03 13:11:00 +0200

    eritrea: add holidays

---
+++ jollyday-tests/src/test/java/de/focus_shift/jollyday/tests/country/HolidayERTest.java
@@ -0,0 +1,38 @@
+package de.focus_shift.jollyday.tests.country;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.Year;
+
+import static de.focus_shift.jollyday.core.HolidayCalendar.ERITREA;
+import static de.focus_shift.jollyday.tests.CalendarCheckerApi.assertFor;
+import static java.time.Month.JANUARY;
+import static java.time.Month.JUNE;
+import static java.time.Month.MARCH;
+import static java.time.Month.MAY;
+import static java.time.Month.SEPTEMBER;
+
+class HolidayERTest {
+
+  private static final Year YEAR_FROM = Year.of(1900);
+  private static final Year YEAR_TO = Year.of(2173);
+
+  @Test
+  void ensuresHolidays() {
+    assertFor(ERITREA)
+      .hasFixedHoliday("NEW_YEAR", JANUARY, 1).validBetween(YEAR_FROM, YEAR_TO).and()
+      .hasFixedHoliday("ORTHODOX_CHRISTMAS", JANUARY, 7).validBetween(YEAR_FROM, YEAR_TO).and()
+      .hasFixedHoliday("EPIPHANY", JANUARY, 19).validBetween(YEAR_FROM, YEAR_TO).and()
+      .hasFixedHoliday("WOMENS_DAY", MARCH, 8).validBetween(YEAR_FROM, YEAR_TO).and()
+      .hasFixedHoliday("LABOUR_DAY", MAY, 1).validBetween(YEAR_FROM, YEAR_TO).and()
+      .hasFixedHoliday("INDEPENDENCE_DAY", MAY, 24).validBetween(YEAR_FROM, YEAR_TO).and()
+      .hasFixedHoliday("MARTYRS_DAY", JUNE, 20).validBetween(YEAR_FROM, YEAR_TO).and()
+      .hasFixedHoliday("REVOLUTION", SEPTEMBER, 1).validBetween(YEAR_FROM, YEAR_TO).and()
+      .hasChristianHoliday("GOOD_FRIDAY").validBetween(YEAR_FROM, YEAR_TO).and()
+      .hasChristianHoliday("EASTER").validBetween(YEAR_FROM, YEAR_TO).and()
+      .hasIslamicHoliday("MAWLID_AN_NABI").validBetween(YEAR_FROM, YEAR_TO).and()
+      .hasIslamicHoliday("ID_AL_FITR").validBetween(YEAR_FROM, YEAR_TO).and()
+      .hasIslamicHoliday("ID_UL_ADHA").validBetween(YEAR_FROM, YEAR_TO)
+      .check();
+  }
+}
