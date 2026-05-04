42c42fa96d47aa7d1529cdfa3b1e6d54b63cf29e
Tobias Schneider <Tobias.Schneider@Tobsch.org>
2026-05-04 13:56:39 +0200

    trinidad and tobago: add holidays

---
+++ jollyday-tests/src/test/java/de/focus_shift/jollyday/tests/country/HolidayTTTest.java
@@ -0,0 +1,70 @@
+package de.focus_shift.jollyday.tests.country;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.Year;
+
+import static de.focus_shift.jollyday.core.HolidayCalendar.TRINIDAD_AND_TOBAGO;
+import static de.focus_shift.jollyday.tests.CalendarCheckerApi.assertFor;
+import static java.time.DayOfWeek.MONDAY;
+import static java.time.DayOfWeek.SUNDAY;
+import static java.time.Month.AUGUST;
+import static java.time.Month.DECEMBER;
+import static java.time.Month.JANUARY;
+import static java.time.Month.JUNE;
+import static java.time.Month.MARCH;
+import static java.time.Month.MAY;
+import static java.time.Month.SEPTEMBER;
+
+class HolidayTTTest {
+
+  private static final Year YEAR_FROM = Year.of(1900);
+  private static final Year YEAR_TO = Year.of(2173);
+
+  @Test
+  void ensuresHolidays() {
+
+    assertFor(TRINIDAD_AND_TOBAGO)
+      .hasFixedHoliday("NEW_YEAR", JANUARY, 1)
+        .canBeMovedFrom(SUNDAY, MONDAY)
+        .validBetween(YEAR_FROM, YEAR_TO)
+      .and()
+      .hasFixedHoliday("SPIRITUAL_BAPTIST_SHOUTER_LIBERATION_DAY", MARCH, 30)
+        .canBeMovedFrom(SUNDAY, MONDAY)
+        .validBetween(YEAR_FROM, YEAR_TO)
+      .and()
+      .hasFixedHoliday("INDIAN_ARRIVAL_DAY", MAY, 30)
+        .canBeMovedFrom(SUNDAY, MONDAY)
+        .validBetween(YEAR_FROM, YEAR_TO)
+      .and()
+      .hasFixedHoliday("LABOUR_DAY", JUNE, 19)
+        .canBeMovedFrom(SUNDAY, MONDAY)
+        .validBetween(YEAR_FROM, YEAR_TO)
+      .and()
+      .hasFixedHoliday("EMANCIPATION_DAY", AUGUST, 1)
+        .canBeMovedFrom(SUNDAY, MONDAY)
+        .validBetween(YEAR_FROM, YEAR_TO)
+      .and()
+      .hasFixedHoliday("INDEPENDENCE_DAY", AUGUST, 31)
+        .canBeMovedFrom(SUNDAY, MONDAY)
+        .validBetween(YEAR_FROM, YEAR_TO)
+      .and()
+      .hasFixedHoliday("REPUBLIC_DAY", SEPTEMBER, 24)
+        .canBeMovedFrom(SUNDAY, MONDAY)
+        .validBetween(YEAR_FROM, YEAR_TO)
+      .and()
+      .hasFixedHoliday("CHRISTMAS", DECEMBER, 25)
+        .canBeMovedFrom(SUNDAY, MONDAY)
+        .validBetween(YEAR_FROM, YEAR_TO)
+      .and()
+      .hasFixedHoliday("BOXING_DAY", DECEMBER, 26)
+        .canBeMovedFrom(SUNDAY, MONDAY)
+        .validBetween(YEAR_FROM, YEAR_TO)
+      .and()
+      .hasChristianHoliday("GOOD_FRIDAY").validBetween(YEAR_FROM, YEAR_TO).and()
+      .hasChristianHoliday("EASTER_MONDAY").validBetween(YEAR_FROM, YEAR_TO).and()
+      .hasChristianHoliday("CORPUS_CHRISTI").validBetween(YEAR_FROM, YEAR_TO).and()
+      .hasIslamicHoliday("ID_AL_FITR").validBetween(YEAR_FROM, YEAR_TO)
+      .check();
+  }
+}
