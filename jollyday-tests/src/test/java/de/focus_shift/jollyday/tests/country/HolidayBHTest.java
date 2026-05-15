21f3547a399e53c73f5ab0bbc3efd74d1a9d0125
Tobias Schneider <Tobias.Schneider@Tobsch.org>
2026-05-15 12:13:58 +0200

    bahrain: add holidays

---
+++ jollyday-tests/src/test/java/de/focus_shift/jollyday/tests/country/HolidayBHTest.java
@@ -0,0 +1,40 @@
+package de.focus_shift.jollyday.tests.country;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.Year;
+
+import static de.focus_shift.jollyday.core.HolidayCalendar.BAHRAIN;
+import static de.focus_shift.jollyday.tests.CalendarCheckerApi.assertFor;
+import static java.time.Month.JANUARY;
+import static java.time.Month.FEBRUARY;
+import static java.time.Month.MAY;
+import static java.time.Month.DECEMBER;
+
+class HolidayBHTest {
+
+  private static final Year YEAR_FROM = Year.of(1900);
+  private static final Year YEAR_TO = Year.of(2173);
+
+  @Test
+  void ensuresHolidays() {
+    assertFor(BAHRAIN)
+      .hasFixedHoliday("NEW_YEAR", JANUARY, 1).validBetween(YEAR_FROM, YEAR_TO).and()
+      .hasFixedHoliday("SPORTS_DAY", FEBRUARY, 22).validBetween(YEAR_FROM, YEAR_TO).and()
+      .hasFixedHoliday("LABOUR_DAY", MAY, 1).validBetween(YEAR_FROM, YEAR_TO).and()
+      .hasFixedHoliday("NATIONAL_DAY", DECEMBER, 16).validBetween(YEAR_FROM, YEAR_TO).and()
+      .hasFixedHoliday("NATIONAL_DAY", DECEMBER, 17).validBetween(YEAR_FROM, YEAR_TO).and()
+      .hasIslamicHoliday("NEWYEAR").validBetween(YEAR_FROM, YEAR_TO).and()
+      .hasIslamicHoliday("TASUA").validBetween(YEAR_FROM, YEAR_TO).and()
+      .hasIslamicHoliday("ASCHURA").validBetween(YEAR_FROM, YEAR_TO).and()
+      .hasIslamicHoliday("MAWLID_AN_NABI").validBetween(YEAR_FROM, YEAR_TO).and()
+      .hasIslamicHoliday("ID_AL_FITR").validBetween(YEAR_FROM, YEAR_TO).and()
+      .hasIslamicHoliday("ID_AL_FITR_2").validBetween(YEAR_FROM, YEAR_TO).and()
+      .hasIslamicHoliday("ID_AL_FITR_3").validBetween(YEAR_FROM, YEAR_TO).and()
+      .hasIslamicHoliday("ARAFAAT").validBetween(YEAR_FROM, YEAR_TO).and()
+      .hasIslamicHoliday("ID_UL_ADHA").validBetween(YEAR_FROM, YEAR_TO).and()
+      .hasIslamicHoliday("ID_UL_ADHA_2").validBetween(YEAR_FROM, YEAR_TO).and()
+      .hasIslamicHoliday("ID_UL_ADHA_3").validBetween(YEAR_FROM, YEAR_TO)
+      .check();
+  }
+}
