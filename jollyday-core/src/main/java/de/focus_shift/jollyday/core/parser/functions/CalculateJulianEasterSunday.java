a640702eff9ba5679ebc8d69f284f5b491cc5b41
Tobias Schneider <Tobias.Schneider@Tobsch.org>
2026-05-28 16:06:02 +0200

    CalculateJulianEasterSunday: improve variable name with semantic meanings

--- jollyday-core/src/main/java/de/focus_shift/jollyday/core/parser/functions/CalculateJulianEasterSunday.java
+++ jollyday-core/src/main/java/de/focus_shift/jollyday/core/parser/functions/CalculateJulianEasterSunday.java
@@ -11,16 +11,14 @@ public class CalculateJulianEasterSunday implements Function<Year, LocalDate> {
 
   @Override
   public @NonNull LocalDate apply(@NonNull final Year year) {
-    int a, b, c, d, e;
-    int x, month, day;
-    a = year.getValue() % 4;
-    b = year.getValue() % 7;
-    c = year.getValue() % 19;
-    d = (19 * c + 15) % 30;
-    e = (2 * a + 4 * b - d + 34) % 7;
-    x = d + e + 114;
-    month = x / 31;
-    day = (x % 31) + 1;
+    final int leapYearCyclePosition = year.getValue() % 4;
+    final int weekDayOffset = year.getValue() % 7;
+    final int metonicCyclePosition = year.getValue() % 19;
+    final int lunarAgeFactor = (19 * metonicCyclePosition + 15) % 30;
+    final int weekDayCorrection = (2 * leapYearCyclePosition + 4 * weekDayOffset - lunarAgeFactor + 34) % 7;
+    final int dayOffset = lunarAgeFactor + weekDayCorrection + 114;
+    final int month = dayOffset / 31;
+    final int day = (dayOffset % 31) + 1;
     return LocalDate.from(JulianChronology.INSTANCE.date(year.getValue(), (month == 3 ? 3 : 4), day));
   }
 }
