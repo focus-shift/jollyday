3f323df4eb8ce996e3a6dc50a8395bc17a1e6010
Tobias Schneider <Tobias.Schneider@Tobsch.org>
2026-07-10 21:36:04 +0200

    saint-martin: add holidays
    closes #1197
    
    closes #1197

---
+++ jollyday-tests/src/test/java/de/focus_shift/jollyday/tests/country/HolidayMFTest.java
@@ -0,0 +1,51 @@
+package de.focus_shift.jollyday.tests.country;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.Year;
+
+import static de.focus_shift.jollyday.core.HolidayCalendar.SAINT_MARTIN;
+import static de.focus_shift.jollyday.tests.CalendarCheckerApi.assertFor;
+import static java.time.Month.AUGUST;
+import static java.time.Month.DECEMBER;
+import static java.time.Month.JANUARY;
+import static java.time.Month.JULY;
+import static java.time.Month.MAY;
+import static java.time.Month.NOVEMBER;
+
+class HolidayMFTest {
+
+  private static final Year YEAR_FROM = Year.of(1900);
+  private static final Year YEAR_TO = Year.of(2173);
+
+  @Test
+  void ensuresHolidays() {
+    assertFor(SAINT_MARTIN)
+      .hasFixedHoliday("NEW_YEAR", JANUARY, 1).validBetween(YEAR_FROM, YEAR_TO).and()
+      .hasFixedHoliday("LABOUR_DAY", MAY, 1).validBetween(YEAR_FROM, YEAR_TO).and()
+      .hasFixedHoliday("VICTORY_DAY", MAY, 8).validBetween(YEAR_FROM, YEAR_TO).and()
+      // Code du travail Art. L3422-2 designates 27 May, inherited from Guadeloupe; superseded
+      // in practice by an interministerial circular of 4 May 2018 which authorized 28 May instead
+      .hasFixedHoliday("ABOLITION_OF_SLAVERY", MAY, 27)
+        .validBetween(YEAR_FROM, Year.of(2017))
+        .notValidBetween(Year.of(2018), YEAR_TO)
+      .and()
+      .hasFixedHoliday("ABOLITION_OF_SLAVERY", MAY, 28)
+        .notValidBetween(YEAR_FROM, Year.of(2017))
+        .validBetween(Year.of(2018), YEAR_TO)
+      .and()
+      .hasFixedHoliday("NATIONAL_DAY", JULY, 14).validBetween(YEAR_FROM, YEAR_TO).and()
+      .hasFixedHoliday("ASSUMPTION_MARY", AUGUST, 15).validBetween(YEAR_FROM, YEAR_TO).and()
+      .hasFixedHoliday("ALL_SAINTS", NOVEMBER, 1).validBetween(YEAR_FROM, YEAR_TO).and()
+      .hasFixedHoliday("ARMISTICE", NOVEMBER, 11).validBetween(YEAR_FROM, YEAR_TO).and()
+      .hasFixedHoliday("CHRISTMAS", DECEMBER, 25).validBetween(YEAR_FROM, YEAR_TO).and()
+      .hasChristianHoliday("EASTER_MONDAY").validBetween(YEAR_FROM, YEAR_TO).and()
+      .hasChristianHoliday("ASCENSION_DAY").validBetween(YEAR_FROM, YEAR_TO).and()
+      // Whit Monday was not a day off from 2004 to 2007 (Journée de solidarité)
+      .hasChristianHoliday("WHIT_MONDAY")
+        .validBetween(YEAR_FROM, Year.of(2003))
+        .notValidBetween(Year.of(2004), Year.of(2007))
+        .validBetween(Year.of(2008), YEAR_TO)
+      .check();
+  }
+}
